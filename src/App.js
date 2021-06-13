847a1bacc936d73360142256389afe806de33efe
amirfakhrullah <amirfakhrullah96@gmail.com>
2021-06-14 00:17:12 +0800

    fixing loader size

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SearchPage from './features/searchPage/searchPage';
 import ResultPage from './features/resultPage/resultPage';
 import SignaturePage from './features/signaturePage/signaturePage';
-import LoadingPage from './features/loadingPage/loadingPage';
 
 export default function App() {
   return (
@@ -19,8 +18,6 @@ export default function App() {
         </Route>
         <Route path={'/user/:id'}>
           <SignaturePage />
-        </Route><Route path={'/loading'}>
-          <LoadingPage />
         </Route>
       </Switch>
     </Router>
