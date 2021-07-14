9c32eeb72cac415376695f83ff23737c56c6d7c6
amirfakhrullah <amirfakhrullah96@gmail.com>
2021-07-14 23:26:58 +0800

    unused useHistory removed

--- src/features/resetPasswordPage/resetPasswordPage.js
+++ src/features/resetPasswordPage/resetPasswordPage.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import { useHistory } from 'react-router-dom';
-
 import { Button, Form } from 'semantic-ui-react';
 
 import { useDispatch, useSelector } from 'react-redux';
